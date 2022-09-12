92f99b154265f972aa4d22ac10cff8c634ebf2c8
Admin <author-main@mail.ru>
2022-09-12 20:06:08 +0300

    Добавлен класс PasswordViewState

--- app/src/main/java/com/training/shoplocal/passwordview/PasswordViewState.kt
+++ app/src/main/java/com/training/shoplocal/passwordview/PasswordViewState.kt
@@ -1,11 +1,24 @@
 package com.training.shoplocal.passwordview
 
 import android.os.Parcelable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.setValue
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
-class PasswordViewState(private var password: String = ""): Parcelable {
+class PasswordViewState {
+    private var password by mutableStateOf("")
     companion object {
         fun getPasswordState(value: String) = PasswordViewState().apply { this.password = value }
+
+        @Suppress("UNCHECKED_CAST")
+        val Saver: Saver<PasswordViewState,Any> = listSaver(
+            save = { listOf(it.password) },
+            restore = {
+                getPasswordState( value = it[0] as String )
+            }
+        )
     }
 }
