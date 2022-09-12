2dbd79d20f8f6cf58f8db6c944fe8f0bf5e3b407
Admin <author-main@mail.ru>
2022-09-12 20:12:29 +0300

    Добавлен класс PasswordViewState

--- app/src/main/java/com/training/shoplocal/passwordview/PasswordView.kt
+++ app/src/main/java/com/training/shoplocal/passwordview/PasswordView.kt
@@ -3,9 +3,9 @@ package com.training.shoplocal.passwordview
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
 fun PasswordView(password: String) {
-    val state = remember { mutableStateOf(password) }
-
+    val state = rememberSaveable(saver = PasswordViewState.Saver) { PasswordViewState.getPasswordState(password) }
 }
