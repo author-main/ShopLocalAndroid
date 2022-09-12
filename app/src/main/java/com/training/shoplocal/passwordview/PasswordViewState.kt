02bf0aace5952f2ae62b0c9dc900d10208f12e0a
Admin <author-main@mail.ru>
2022-09-12 20:48:21 +0300

    Добавлен класс PasswordViewState

--- app/src/main/java/com/training/shoplocal/passwordview/PasswordViewState.kt
+++ app/src/main/java/com/training/shoplocal/passwordview/PasswordViewState.kt
@@ -9,10 +9,28 @@ import androidx.compose.runtime.setValue
 import kotlinx.parcelize.Parcelize
 
 class PasswordViewState {
-    private val emptyChar   = "○"
-    private val fillChar    = "●"
+    private val emptyChar   = '○'
+    private val fillChar    = '●'
     private var password by mutableStateOf("")
+    fun changePassword(value: String) {
+        password = value
+    }
+
+    fun getPasswordChar(): CharArray {
+        val array = CharArray(PASSWORD_LENGTH)
+        for (index in 0..4)
+            array[index] = emptyChar
+
+        password.forEachIndexed { index, _ ->
+            array[index] = fillChar
+        }
+        return array
+    }
+
+
+
     companion object {
+        const val PASSWORD_LENGTH = 5
         fun getPasswordState(value: String) = PasswordViewState().apply { this.password = value }
 
         @Suppress("UNCHECKED_CAST")
