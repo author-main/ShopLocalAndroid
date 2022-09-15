f2acc5f17fe1e75fc9c85a6fc0eedf077d47c4a6
author-main <author-main@mail.ru>
2022-09-15 11:54:37 +0300

    Добавление анимации

--- app/src/main/java/com/training/shoplocal/passwordview/PasswordView.kt
+++ app/src/main/java/com/training/shoplocal/passwordview/PasswordView.kt
@@ -1,5 +1,7 @@
 package com.training.shoplocal.passwordview
 
+import android.graphics.Paint
+import android.text.Layout
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearEasing
@@ -8,16 +10,18 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Vertical
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.training.shoplocal.ui.theme.TextLightGray
@@ -25,7 +29,15 @@ import com.training.shoplocal.ui.theme.TextOrange
 
 @Composable
 fun PasswordView(state: PasswordViewState) {
-    var visible = MutableTransitionState(false)
+    @Composable
+    fun textChar(value: Char, color: Color){
+        Text(
+            text = value.toString(),
+            fontSize = 27.sp,
+            color = color
+        )
+    }
+    val visible = MutableTransitionState(false)
     val passwordState = rememberSaveable(saver = PasswordViewState.Saver) { state }
     val chars = passwordState.getPasswordChar()
     val indexChar = chars.lastIndexOf(PasswordViewState.fillChar)
@@ -48,25 +60,25 @@ fun PasswordView(state: PasswordViewState) {
             }
             else*/
 
-
-            if (passwordState.isAnimated() && index == indexChar)
-            AnimatedVisibility(visibleState = visible,
-                enter = fadeIn(animationSpec = tween(durationMillis = 1300, easing = LinearEasing))) {
-                Text(
-                    text = chars[index].toString(),
-                    modifier = Modifier
-                        .padding(8.dp),
-                    fontSize = 27.sp,
-                    color = textColor
-                )
+            Box(modifier = Modifier
+                .size(32.dp, 36.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                //val duration = if (passwordState.isAnimated() && index == indexChar) 300 else 0
+                if (passwordState.isAnimated() && index == indexChar)
+                    androidx.compose.animation.AnimatedVisibility(
+                        visibleState = visible,
+                        enter = fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 200,
+                                easing = LinearEasing
+                            )
+                        )
+                    ) {
+                        textChar(value = chars[index], color = textColor)
+                    }
+                 else textChar(value = chars[index], color = textColor)
             }
-            else Text(
-                text = chars[index].toString(),
-                modifier = Modifier
-                    .padding(8.dp),
-                fontSize = 27.sp,
-                color = textColor
-            )
         }
     }
     SideEffect {
