86118c698cd9a18d46fa00a8a393b583c5774d46
Admin <author-main@mail.ru>
2022-09-16 20:28:10 +0300

    Create Animation

---
+++ app/src/main/java/com/training/shoplocal/buttonpanel/ButtonPanel.kt
@@ -0,0 +1,9 @@
+package com.training.shoplocal.buttonpanel
+
+import androidx.compose.runtime.Composable
+import com.training.shoplocal.passwordview.PasswordViewState
+
+@Composable
+fun ButtonPanel(state: PasswordViewState){
+
+}
