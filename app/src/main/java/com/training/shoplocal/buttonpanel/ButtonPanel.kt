45352f80aca54cb8a3e7de81e18c56930aabb6d2
Admin <author-main@mail.ru>
2022-09-18 19:44:26 +0300

    Create Buttons

--- app/src/main/java/com/training/shoplocal/buttonpanel/ButtonPanel.kt
+++ app/src/main/java/com/training/shoplocal/buttonpanel/ButtonPanel.kt
@@ -33,7 +33,6 @@ fun ButtonPanel(changeChar: (value: Char)-> Unit){
                         shape = CircleShape,
                         contentPadding = PaddingValues(0.dp),
                     ) {
-
                         val st = i * 3 + j
                         val caption = st.toString()
                         Text(
