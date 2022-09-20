6024e278dc467cc32d1f8f9be29b73d7672c8857
author-main <author-main@mail.ru>
2022-09-20 11:01:35 +0300

    Add Buttons and EMail

--- app/src/main/java/com/training/shoplocal/buttonpanel/ButtonPanel.kt
+++ app/src/main/java/com/training/shoplocal/buttonpanel/ButtonPanel.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,10 +25,11 @@ import com.training.shoplocal.passwordview.PasswordViewState
 import com.training.shoplocal.ui.theme.TextLightGray
 import kotlinx.coroutines.delay
 import com.training.shoplocal.R
+import com.training.shoplocal.ui.theme.TextOrange
 
 @Composable
 fun ButtonPanel(changeChar: (value: Char)-> Unit){
-    Column(){
+    Column(horizontalAlignment = Alignment.CenterHorizontally){
         for (i in 0..3) {
             Row(){
                 for (j in 1..3 ) {
@@ -49,27 +52,61 @@ fun ButtonPanel(changeChar: (value: Char)-> Unit){
                     OutlinedButton(
                         onClick = {changeChar.invoke(char)},
                         modifier = Modifier.size(64.dp, 64.dp),
-                        border = BorderStroke(1.dp, Color.Transparent),
+                        border = null,
                         shape = CircleShape,
                         contentPadding = PaddingValues(0.dp),
                     ) {
                         if (char != ' ' && char != '<')
                             Text(
                                 text = char.toString(),
-                                fontSize = 36.sp,
+                                fontSize = 32.sp,
                                 color = TextLightGray,
                                 fontWeight = FontWeight.Light
                             )
                         else Image(imageVector = ImageVector.vectorResource(image),
-                            modifier = Modifier.size(28.dp, 28.dp),
+                            modifier = Modifier.size(24.dp, 24.dp),
                             contentDescription = null)
                     }
                     if (j<3)
-                        Spacer(modifier = Modifier.width(32.dp))
+                        Spacer(modifier = Modifier.width(16.dp))
                 }
             }
-            if (i<3)
-                Spacer(modifier = Modifier.height(8.dp))
+          /*  if (i<3)
+                Spacer(modifier = Modifier.height(8.dp))*/
+        }
+        Row(verticalAlignment = Alignment.CenterVertically){
+            OutlinedButton(
+                onClick = {},
+                border = null,
+                shape = CircleShape,
+          //     contentPadding = PaddingValues(8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.btn_reg),
+                    color = TextOrange,
+                    fontSize = 13.sp
+                )
+            }
+
+            Text(
+                text = "|",
+                color = TextLightGray,
+                fontSize = 14.sp
+            )
+
+            OutlinedButton(
+                onClick = {},
+                border = null,
+                shape = CircleShape,
+                //contentPadding = PaddingValues(8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.btn_rest),
+                    color = TextOrange,
+                    fontSize = 13.sp
+                )
+            }
+
         }
     }
 
