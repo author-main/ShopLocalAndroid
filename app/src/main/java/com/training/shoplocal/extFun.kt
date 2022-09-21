098b0fdb928f23853366e972db0b569c061b6bd4
author-main <author-main@mail.ru>
2022-09-21 13:16:05 +0300

    Add Buttons and EMail

--- app/src/main/java/com/training/shoplocal/extFun.kt
+++ app/src/main/java/com/training/shoplocal/extFun.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 const val defaultStringResource = ""
 fun getStringResource(@StringRes id: Int): String =
     try {
+
         AppShopLocal.appContext().getString(id)
     }
     catch (e: Exception){
