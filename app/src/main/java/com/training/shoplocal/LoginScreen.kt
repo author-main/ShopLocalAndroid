055478481efc234fa2b00851aac2b2dbc20be340
author-main <author-main@mail.ru>
2022-09-22 13:13:12 +0300

    Add LocalShop.svg

--- app/src/main/java/com/training/shoplocal/LoginScreen.kt
+++ app/src/main/java/com/training/shoplocal/LoginScreen.kt
@@ -1,11 +1,13 @@
 package com.training.shoplocal
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -37,9 +39,14 @@ fun LoginScreen(state: LoginViewState){
                 //fontSize = 13.sp
             )
 
-            LoginView(state)
-            Spacer(modifier = Modifier.height(8.dp))
-            ButtonPasswordPanel(state::changeChar)
+            BoxWithConstraints(Modifier.background(Color.Red)
+                .fillMaxHeight()) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    LoginView(state)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    ButtonPasswordPanel(state::changeChar)
+                }
+            }
 
         }
     }
