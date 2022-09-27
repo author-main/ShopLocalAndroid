ea6d7e4c6e1a6b0a912265e353d37f09c27ada0e
author-main <author-main@mail.ru>
2022-09-27 12:43:32 +0300

    27/09/2022 потеря фокуса при выводе диалога

--- app/src/main/java/com/training/shoplocal/dialogs/DialogRestore.kt
+++ app/src/main/java/com/training/shoplocal/dialogs/DialogRestore.kt
@@ -5,28 +5,46 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.training.shoplocal.DialogRouter
 import com.training.shoplocal.R
 import com.training.shoplocal.ui.theme.*
+import com.training.shoplocal.validateMail
 import com.training.shoplocal.viewmodel.RepositoryViewModel
 
 @Composable
 fun DialogRestore(){
     val viewModel: RepositoryViewModel = viewModel()
+    val email = remember{mutableStateOf("")}
+  /*  Dialog(onDismissRequest = { }) {
+        
+    }*/
+    
+    
     AlertDialog(backgroundColor = Color.DarkGray,
         contentColor = TextLightGray,//contentColorFor(backgroundColor),
         shape = RoundedCornerShape(16.dp),
@@ -34,7 +52,38 @@ fun DialogRestore(){
            DialogRouter.reset()
         },
         title = { Text(text = stringResource(R.string.title_restore))},
-        text = { Text("Вы действительно хотите удалить выбранный элемент?") },
+        text = {
+            Column() {
+                //Spacer(modifier = Modifier.height(64.dp))
+                TextField(
+                    value = email.value,
+                    onValueChange = {
+                        email.value = it
+                    },
+                    Modifier
+                        .fillMaxWidth(),
+                    trailingIcon = {
+                        /*  if (errorEmail.value)
+                        Icon(Icons.Filled.Email, contentDescription = "", tint = SelectedItem)
+                    else
+                        Icon(Icons.Filled.Email, contentDescription = "")*/
+                    },
+                    textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = TextFieldBg,
+                        cursorColor = TextFieldFont,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                )
+
+
+
+            }
+        },
         buttons = {
             Row(
                 Modifier
@@ -67,7 +116,7 @@ fun DialogRestore(){
                 ) {
                     Text(stringResource(id = R.string.btn_send).uppercase(), color = TextOrange)
                 }
-                
+
             }
         }
     )
