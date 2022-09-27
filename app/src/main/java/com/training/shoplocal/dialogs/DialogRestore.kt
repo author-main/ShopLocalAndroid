ae38330c11a867df42476c975fb628a9c671a8ec
author-main <author-main@mail.ru>
2022-09-27 10:45:33 +0300

    27/09/2022 потеря фокуса при выводе диалога

--- app/src/main/java/com/training/shoplocal/dialogs/DialogRestore.kt
+++ app/src/main/java/com/training/shoplocal/dialogs/DialogRestore.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.DialogProperties
@@ -71,3 +72,9 @@ fun DialogRestore(){
         }
     )
 }
+
+/*@Preview(showBackground = true)
+@Composable
+fun DialogRestorePreview() {
+    DialogRestore()
+}*/
