c473e5b7bb9f2b48c5e2690cad7312e6e9948753
author-main <author-main@mail.ru>
2022-10-15 09:06:47 +0300

    15/10/2022 Завершено операции с пользователем

--- app/src/main/java/com/training/shoplocal/dialogs/ShowMessage.kt
+++ app/src/main/java/com/training/shoplocal/dialogs/ShowMessage.kt
@@ -40,21 +40,6 @@ fun <T:ViewModel> ShowMessage(message: String, type: MESSAGE = MESSAGE.INFO, vie
     //val scope = rememberCoroutineScope()
     val labelFont = FontFamily(Font(R.font.robotocondensed_light))
     val snackbarHostState = remember { mutableStateOf(SnackbarHostState()) }
-    var color = TextFieldBg
-    var image = R.drawable.ic_notifications
-    when (type) {
-        MESSAGE.INFO ->{
-            //color = SelectedItem//.copy(alpha = 0.7f)
-        }
-        MESSAGE.WARNING ->{
-            //color = TextOrange
-            image = R.drawable.ic_warning
-        }
-        MESSAGE.ERROR ->{
-            color = SelectedItem//.copy(alpha = 0.7f)
-            image = R.drawable.ic_error
-        }
-    }
 
     LaunchedEffect(null) {
         when (snackbarHostState.value.showSnackbar(message, duration = SnackbarDuration.Short)) {
@@ -76,7 +61,7 @@ fun <T:ViewModel> ShowMessage(message: String, type: MESSAGE = MESSAGE.INFO, vie
                     Card(
                         shape = RoundedCornerShape(16.dp),
                         //border = BorderStroke(1.dp, TextFieldFont),
-                        backgroundColor = color,//TextFieldBg,
+                        backgroundColor = type.color,//TextFieldBg,
                         modifier = Modifier
                             .padding(16.dp)
                             .wrapContentSize()
@@ -93,7 +78,7 @@ fun <T:ViewModel> ShowMessage(message: String, type: MESSAGE = MESSAGE.INFO, vie
                                 //.border(width = 2.dp, color = TextLightGray, CircleShape)
                                 //.padding(8.dp),
                                 //imageVector = Icons.Default.Notifications, contentDescription = ""
-                                imageVector = ImageVector.vectorResource(image),
+                                imageVector = ImageVector.vectorResource(type.icon),
                                 contentDescription = ""
                             )
                             Text(
