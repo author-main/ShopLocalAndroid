13839eb3786ee89e65c55643e8b20ed7b50b8d0e
Admin <author-main@mail.ru>
2022-10-17 19:53:43 +0300

    17/10/2022 начало реализации MainScreen()

--- app/src/main/java/com/training/shoplocal/screens/BottomNavigationItem.kt
+++ app/src/main/java/com/training/shoplocal/screens/BottomNavigationItem.kt
@@ -1,6 +1,8 @@
 package com.training.shoplocal.screens
 
 import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import com.training.shoplocal.AppShopLocal
 import com.training.shoplocal.R
 import com.training.shoplocal.TEXT_BOTTOMNAVIGATION
@@ -13,9 +15,19 @@ enum class BottomNavigationItemData(@DrawableRes var icon: Int, val iconOn: Int?
     PROFILE     (R.drawable.ic_error,     R.drawable.ic_error,    TEXT_BOTTOMNAVIGATION[3])
 }
 
-sealed class BottomNavigationItem(val data: BottomNavigationItemData){
-    object Main:        BottomNavigationItem(BottomNavigationItemData.MAIN)
-    object Catalog:     BottomNavigationItem(BottomNavigationItemData.CATALOG)
-    object Cart:        BottomNavigationItem(BottomNavigationItemData.CART)
-    object Profile:     BottomNavigationItem(BottomNavigationItemData.PROFILE)
+sealed class BottomNavigationItem(val route: String, val data: BottomNavigationItemData){
+    object Main:        BottomNavigationItem("route_main", BottomNavigationItemData.MAIN)
+    object Catalog:     BottomNavigationItem("route_catalog", BottomNavigationItemData.CATALOG)
+    object Cart:        BottomNavigationItem("route_cart", BottomNavigationItemData.CART)
+    object Profile:     BottomNavigationItem("route_profile", BottomNavigationItemData.PROFILE)
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val bottomNavigationItems = listOf(
+        BottomNavigationItem.Main,
+        BottomNavigationItem.Catalog,
+        BottomNavigationItem.Cart,
+        BottomNavigationItem.Profile
+    )
 }
--- app/src/main/java/com/training/shoplocal/screens/MainScreen.kt
+++ app/src/main/java/com/training/shoplocal/screens/MainScreen.kt
@@ -1,16 +1,44 @@
 package com.training.shoplocal.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.training.shoplocal.ScreenItem
 
 @Composable
 fun MainScreen(){
-    val bottomNavigationItems = listOf(
-        BottomNavigationItem.Main,
-        BottomNavigationItem.Catalog,
-        BottomNavigationItem.Cart,
-        BottomNavigationItem.Profile
-    )
     val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController)
+        }
+    ) {
+        Box(modifier = Modifier.padding(it)) {
+            Navigation(navController)
+        }
+    }
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavigationItem.Main.route) {
+        composable(BottomNavigationItem.Main.data.text) {
+            //BooksScreen()
+        }
+        composable(BottomNavigationItem.Catalog.route) {
+            //DialogRouter.navigateTo(Dialog.Color)
+            //log("navigation search update")
+        }
+        composable(BottomNavigationItem.Cart.route) {
+            //DialogRouter.navigateTo(Dialog.InsertButtons)
+        }
+        composable(BottomNavigationItem.Profile.route) {
+        }
+    }
 }
