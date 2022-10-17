0d58f9a158cdb7b7dc8e25396e13d09f342117d2
Admin <author-main@mail.ru>
2022-10-17 20:36:08 +0300

    17/10/2022 начало реализации MainScreen()

--- app/src/main/java/com/training/shoplocal/screens/BottomNavigationItem.kt
+++ app/src/main/java/com/training/shoplocal/screens/BottomNavigationItem.kt
@@ -1,11 +1,18 @@
 package com.training.shoplocal.screens
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.training.shoplocal.AppShopLocal
 import com.training.shoplocal.R
 import com.training.shoplocal.TEXT_BOTTOMNAVIGATION
+import com.training.shoplocal.log
 
 
 enum class BottomNavigationItemData(@DrawableRes var icon: Int, val iconOn: Int? = null, val text: String) {
@@ -30,4 +37,23 @@ fun BottomNavigationBar(navController: NavController) {
         BottomNavigationItem.Cart,
         BottomNavigationItem.Profile
     )
+    BottomNavigation(
+        /*  Modifier.onGloballyPositioned {
+              log("${it.size.height}")
+          },*/
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        bottomNavigationItems.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(painterResource(id = item.data.icon), contentDescription = null)
+                },
+                label = { Text(item.data.text) },
+                onClick = {},
+                selected = false
+            )
+        }
+    }
 }
