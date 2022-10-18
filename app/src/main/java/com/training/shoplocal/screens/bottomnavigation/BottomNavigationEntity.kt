f653c84f25e08eb18ef895fb7945130a148d0d6c
Admin <author-main@mail.ru>
2022-10-18 19:57:13 +0300

    17/10/2022 начало реализации MainScreen()

--- app/src/main/java/com/training/shoplocal/screens/bottomnavigation/BottomNavigationEntity.kt
+++ app/src/main/java/com/training/shoplocal/screens/bottomnavigation/BottomNavigationEntity.kt
@@ -1,12 +1,21 @@
 package com.training.shoplocal.screens.bottomnavigation
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -18,11 +27,11 @@ import com.training.shoplocal.ui.theme.TextFieldFont
 import com.training.shoplocal.ui.theme.TextOrange
 
 
-enum class BottomNavigationItemData(@DrawableRes var icon: Int, val iconOn: Int? = null, val text: String) {
-    MAIN        (R.drawable.ic_error,     R.drawable.ic_error,    TEXT_BOTTOMNAVIGATION[0]),
-    CATALOG     (R.drawable.ic_error,     R.drawable.ic_error,    TEXT_BOTTOMNAVIGATION[1]),
-    CART        (R.drawable.ic_error,     R.drawable.ic_error,    TEXT_BOTTOMNAVIGATION[2]),
-    PROFILE     (R.drawable.ic_error,     R.drawable.ic_error,    TEXT_BOTTOMNAVIGATION[3])
+enum class BottomNavigationItemData(@DrawableRes var icon: Int, val text: String) {
+    MAIN        (R.drawable.ic_main,        TEXT_BOTTOMNAVIGATION[0]),
+    CATALOG     (R.drawable.ic_catalog,     TEXT_BOTTOMNAVIGATION[1]),
+    CART        (R.drawable.ic_cart,        TEXT_BOTTOMNAVIGATION[2]),
+    PROFILE     (R.drawable.ic_profile,     TEXT_BOTTOMNAVIGATION[3])
 }
 
 sealed class BottomNavigationItem(val route: String, val data: BottomNavigationItemData){
@@ -31,6 +40,33 @@ sealed class BottomNavigationItem(val route: String, val data: BottomNavigationI
     object Cart:        BottomNavigationItem("route_cart", BottomNavigationItemData.CART)
     object Profile:     BottomNavigationItem("route_profile", BottomNavigationItemData.PROFILE)
 }
+
+
+@Composable
+fun IconWithText(value: BottomNavigationItemData, selected: Boolean = false,
+                 interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+                 action: ()->Unit = {}){
+    val labelFont = FontFamily(Font(R.font.robotocondensed_light))
+    Column(Modifier.fillMaxHeight()
+        .padding(horizontal = 24.dp)
+        .clickable(
+            interactionSource = interactionSource,
+            indication = null) {
+                action.invoke()
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        val color = if (selected) TextOrange
+                        else TextFieldFont
+            Icon(painterResource(id = value.icon),
+                tint = color,
+                contentDescription = null)
+            Text(value.text, color = color, fontSize = 13.sp, fontFamily = labelFont)
+    }
+}
+
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
@@ -48,14 +84,17 @@ fun BottomNavigationBar(navController: NavController) {
     ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
-      /*  Box(){
-            Row() {
-                bottomNavigationItems.forEach { item ->
-                    Text(item.data.text)
-                }
-            }
-        }*/
         bottomNavigationItems.forEach { item ->
+            val selected = currentRoute == item.route
+            IconWithText(value = item.data, selected, action = {})
+        }
+          /*  Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)) {
+                bottomNavigationItems.forEach { item ->
+                    Text(text = item.data.text)
+                }
+            }*/
+        /*bottomNavigationItems.forEach { item ->
             val selected = currentRoute == item.route
            /* val color = if (selected)
                 TextOrange
@@ -77,7 +116,7 @@ fun BottomNavigationBar(navController: NavController) {
                 unselectedContentColor = TextFieldFont,
                 alwaysShowLabel = true
             )
-        }
+        }*/
     }
 }
 
