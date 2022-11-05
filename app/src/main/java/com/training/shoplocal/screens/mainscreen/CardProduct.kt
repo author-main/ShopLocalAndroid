91d50407a391949a54338379d1b0f73ca18cb685
Admin <author-main@mail.ru>
2022-11-05 15:34:10 +0300

    02/11/2022 компоновка CardProduct

--- app/src/main/java/com/training/shoplocal/screens/mainscreen/CardProduct.kt
+++ app/src/main/java/com/training/shoplocal/screens/mainscreen/CardProduct.kt
@@ -3,33 +3,31 @@ package com.training.shoplocal.screens.mainscreen
 import android.graphics.Bitmap
 import com.training.shoplocal.R
 import android.graphics.BitmapFactory
-import android.media.Image
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.StrokeCap.Companion.Round
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -47,6 +45,45 @@ import java.io.IOException
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
+
+@Composable
+private fun BottomSheetItem(@DrawableRes id: Int, text: String, action: ()->Unit){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = {action()})
+        .height(54.dp)
+        .background(color = PrimaryDark)
+        .padding(start = 15.dp), verticalAlignment = Alignment.CenterVertically){
+        Icon(painterResource(id = id), contentDescription = null, tint = TextFieldFont)
+        Text(modifier = Modifier.padding(start = 16.dp), text = text, color = TextFieldFont)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BotomSheetItemPreview(){
+    BottomSheetItem(id = R.drawable.ic_product_bs, text = "checkit") {
+
+    }
+}
+
+
+@Composable
+private fun BottomSheetProduct(){
+    val textItems = stringArrayResource(id = R.array.bottomsheet_product_items)
+    BottomSheetItem(R.drawable.ic_brend_bs, textItems[0]){
+
+    }
+    BottomSheetItem(R.drawable.ic_favorite_bs, textItems[1]){
+
+    }
+    BottomSheetItem(R.drawable.ic_product_bs, textItems[2]){
+
+    }
+    BottomSheetItem(R.drawable.ic_cart, textItems[3]){
+
+    }
+}
 
 @Composable
 fun DiscountPanel(modifier: Modifier, percent: Int){
