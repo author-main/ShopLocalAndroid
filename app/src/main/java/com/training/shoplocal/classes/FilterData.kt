c724ffa3a3db5c9e8eb536cd900940a512d6b932
author-main <author-main@mail.ru>
2022-11-07 14:12:27 +0300

    03/11/2022 оформление CardProduct

--- app/src/main/java/com/training/shoplocal/classes/FilterData.kt
+++ app/src/main/java/com/training/shoplocal/classes/FilterData.kt
@@ -6,6 +6,7 @@ import com.training.shoplocal.log
   Класс для выборки данных (product) из БД (таблица products),
  * определяет тип сортировки и фильтр выборки по параметрам продукта
  */
+
 const val ANY_VALUE =    -1
 enum class SORT_TYPE     {ASCENDING, DESCENDING}
 enum class SORT_PROPERTY {POPULAR, RATING, PRICE}
