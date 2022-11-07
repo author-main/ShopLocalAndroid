2807aa9cd1cb10b88087cfb48c6adc1d6209a3f7
author-main <author-main@mail.ru>
2022-11-07 13:47:43 +0300

    03/11/2022 оформление CardProduct

--- app/src/main/java/com/training/shoplocal/classes/SelectedData.kt
+++ app/src/main/java/com/training/shoplocal/classes/SelectedData.kt
@@ -7,58 +7,106 @@ package com.training.shoplocal.classes
 const val ANY_VALUE =    -1
 enum class SORT_TYPE     {ASCENDING, DESCENDING}
 enum class SORT_PROPERTY {POPULAR, RATING, PRICE}
-class FilterData(
-    var sortType: SORT_TYPE             = SORT_TYPE.ASCENDING,
-    var sortProperty: SORT_PROPERTY     = SORT_PROPERTY.PRICE,
-    var brend: Int                      = ANY_VALUE,
-    var favorite: Int                   = ANY_VALUE,
-    var priceRange: Pair<Float, Float>
+
+data class FilterData(
+    private var sortType: SORT_TYPE             = SORT_TYPE.ASCENDING,
+    private var sortProperty: SORT_PROPERTY     = SORT_PROPERTY.PRICE,
+    private var brend: Int                      = ANY_VALUE,
+    private var favorite: Int                   = ANY_VALUE,
+    private var priceRange: Pair<Float, Float>
                                         = 0.00f to 0.00f,
-    var category: Int                   = ANY_VALUE
+    private var category: Int                   = ANY_VALUE
     ) {
-    fun isFiltered() : Boolean {
-        return brend != ANY_VALUE || favorite != ANY_VALUE || category != ANY_VALUE || priceRange.second != 0.00f
-    }
+        fun setSortType(value: SORT_TYPE){
+            sortType = value
+        }
+        fun setSortProperty(value: SORT_PROPERTY){
+            sortProperty = value
+        }
+        fun setBrend(value: Int){
+            brend = value
+        }
+        fun setFavorite(value: Int){
+            favorite = value
+        }
+        fun setPriceRange(value: Pair<Float, Float>){
+            priceRange = value
+        }
+        fun setCategory(value: Int){
+            category = value
+        }
+        fun getSortType()       = sortType
+        fun getSortProperty()   = sortProperty
+        fun getBrend()          = brend
+        fun getFavorite()       = favorite
+        fun getPriceRange()     = priceRange
+        fun getCategory()       = category
+        fun isFiltered() : Boolean {
+            return brend != ANY_VALUE || favorite != ANY_VALUE || category != ANY_VALUE || priceRange.second != 0.00f
+        }
 }
 
 interface FilterDataBuilder {
-    fun addBrend(brend: Int):                  FilterDataBuilder?
-    fun addFavorite(value: Boolean):           FilterDataBuilder?
-    fun addPriceRange(from: Int, to: Int):     FilterDataBuilder?
-    fun addCategory(category: Int):            FilterDataBuilder?
-    fun default():                             FilterDataBuilder?
-    fun build():                               FilterData?
+    fun addSortType(sortType: SORT_TYPE):               FilterDataBuilder
+    fun addSortProperty(sortProperty: SORT_PROPERTY):   FilterDataBuilder
+    fun addBrend(brend: Int):                           FilterDataBuilder
+    fun addFavorite(favorite: Int):                     FilterDataBuilder
+    fun addPriceRange(from: Float, to: Float):          FilterDataBuilder
+    fun addCategory(category: Int):                     FilterDataBuilder
+    fun default():                                      FilterDataBuilder
+    fun build():                                        FilterData
 }
 
-class FilterProduct: FilterDataBuilder {
-    private var sortType: SORT_TYPE             = SORT_TYPE.ASCENDING
-    private var sortProperty: SORT_PROPERTY     = SORT_PROPERTY.PRICE
-    private var brend: Int                      = ANY_VALUE
-    private var favorite: Int                   = ANY_VALUE
-    private var priceRange: Pair<Float, Float>  = 0.00f to 0.00f
-    private var category: Int                   = ANY_VALUE
-
-    override fun addBrend(brend: Int): FilterDataBuilder? {
-        TODO("Not yet implemented")
+class ProductBuilder: FilterDataBuilder {
+ /*   init {
+        instance = ProductBuilder()
+    }*/
+    private val filterdata = FilterData()
+    override fun addSortType(sortType: SORT_TYPE): FilterDataBuilder {
+        filterdata.setSortType(sortType)
+        return this
     }
 
-    override fun addFavorite(value: Boolean): FilterDataBuilder? {
-        TODO("Not yet implemented")
+    override fun addSortProperty(sortProperty: SORT_PROPERTY): FilterDataBuilder {
+        filterdata.setSortProperty(sortProperty)
+        return this
     }
 
-    override fun addPriceRange(from: Int, to: Int): FilterDataBuilder? {
-        TODO("Not yet implemented")
+    override fun addBrend(brend: Int): FilterDataBuilder {
+        filterdata.setBrend(brend)
+        return this
     }
 
-    override fun addCategory(category: Int): FilterDataBuilder? {
-        TODO("Not yet implemented")
+    override fun addFavorite(favorite: Int): FilterDataBuilder {
+        filterdata.setFavorite(favorite)
+        return this
     }
 
-    override fun default(): FilterDataBuilder? {
-        TODO("Not yet implemented")
+    override fun addPriceRange(from: Float, to: Float): FilterDataBuilder {
+        filterdata.setPriceRange(Pair(from, to))
+        return this
     }
 
-    override fun build(): FilterData? {
-        TODO("Not yet implemented")
+    override fun addCategory(category: Int): FilterDataBuilder {
+        filterdata.setCategory(category)
+        return this
     }
+
+    override fun default(): FilterDataBuilder {
+        with(filterdata){
+            /*setSortType(SORT_TYPE.ASCENDING)
+            setSortProperty(SORT_PROPERTY.PRICE)*/
+            setBrend(ANY_VALUE)
+            setFavorite(ANY_VALUE)
+            setPriceRange(0.00f to 0.00f)
+            setCategory(ANY_VALUE)
+        }
+        return this
+    }
+
+    override fun build() = filterdata
 }
+
+/*class EasyTest(){
+    val filterProduct = ProductBuilder().addBrend(2).build()
+}*/
