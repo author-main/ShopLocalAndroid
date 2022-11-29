29edb1059c514695cf400eff5b21549646c0e9af
Admin <author-main@mail.ru>
2022-11-29 21:46:33 +0300

    28/11/2022 реализация CardProduct

--- app/src/main/java/com/training/shoplocal/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/com/training/shoplocal/screens/mainscreen/MainScreen.kt
@@ -47,13 +47,14 @@ fun MainScreen(state: ModalBottomSheetState){
                 //CardProduct(products[0], state = state)
 
                 LazyVerticalGrid(modifier = Modifier.fillMaxSize()
-                    .padding(8.dp),
+                    .padding(10.dp),
                     columns = GridCells.Adaptive(minSize = 150.dp),
                     state = rememberLazyGridState(),
-                    contentPadding = PaddingValues(10.dp)
+                   // contentPadding = PaddingValues(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     items(products.size - 1) { index ->
-                            CardProduct(products[index], state = state)
+                        CardProduct(products[index], state = state)
                     }
                 }
 
