d9ede3a652227bf0de03b4ca7122eeb1822e4983
author-main <author-main@mail.ru>
2023-01-21 10:23:01 +0300

    21/01/2023 реализация Filter

--- app/src/main/java/com/training/shoplocal/screens/appscreen/ShowFIlterDisplay.kt
+++ app/src/main/java/com/training/shoplocal/screens/appscreen/ShowFIlterDisplay.kt
@@ -1,12 +1,13 @@
 package com.training.shoplocal.screens.appscreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.training.shoplocal.ui.theme.BgScreenDark
 import com.training.shoplocal.ui.theme.PrimaryDark
 import com.training.shoplocal.ui.theme.TextFieldBg
@@ -17,6 +18,35 @@ fun ShowFilterDisplay(){
         .fillMaxSize()
         .background(BgScreenDark)
     ){
-
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp)) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.1f)
+                ,
+                //elevation = 4.dp,
+                //shape = RoundedCornerShape(20.dp),
+                backgroundColor = PrimaryDark,
+            ) {
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.1f),
+                backgroundColor = PrimaryDark
+            ) {
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.8f),
+                backgroundColor = PrimaryDark
+            ) {
+            }
+        }
     }
 }
