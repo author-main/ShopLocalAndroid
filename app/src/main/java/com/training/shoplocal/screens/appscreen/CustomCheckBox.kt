2c4e6a11f99b5652d183ef731d5fc9f34379dae0
Admin <author-main@mail.ru>
2023-01-23 21:34:50 +0300

    23/01/2023 реализация Filter

--- app/src/main/java/com/training/shoplocal/screens/appscreen/CustomCheckBox.kt
+++ app/src/main/java/com/training/shoplocal/screens/appscreen/CustomCheckBox.kt
@@ -1,8 +1,19 @@
 package com.training.shoplocal.screens.appscreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.training.shoplocal.ui.theme.TextBrand
@@ -10,13 +21,38 @@ import com.training.shoplocal.ui.theme.TextFieldBg
 import com.training.shoplocal.ui.theme.TextFieldFont
 import com.training.shoplocal.ui.theme.TextPrice
 
-data class CheckedStyle(var border: Dp = 1.dp, var borderColor: Color = TextFieldFont, var backgroundColor: Color = TextFieldBg,
-                        var checkmarkColor: Color = TextPrice, var selectedBackgroundColor: Color = TextBrand)
+data class CheckedStyle(var border: Dp = 2.dp, var borderColor: Color = TextFieldFont, var backgroundColor: Color = TextFieldBg,
+                        var checkmarkColor: Color = TextPrice, var selectedBackgroundColor: Color = TextBrand,
+                        var textColor: Color = TextFieldFont)
 
 @Composable
-fun CustomCheckBox(style: CheckedStyle = CheckedStyle(), checked: Boolean = false, onChecked: (value: Boolean) -> Unit){
-    var checkedState by remember {
-        mutableStateOf(false)
+fun CustomCheckBox(style: CheckedStyle = CheckedStyle(), text: String, checked: Boolean = false, onChecked: (value: Boolean) -> Unit){
+    var isChecked by remember {
+        mutableStateOf(checked)
     }
-    checkedState = checked
+    Row(modifier = Modifier
+        .toggleable(value = isChecked,role = Role.Checkbox){
+            isChecked = it
+        },
+            verticalAlignment = Alignment.CenterVertically) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .requiredSize(15.dp)
+                .background(if (isChecked) style.selectedBackgroundColor else style.borderColor)
+                .padding(if (isChecked) 0.dp else style.border)
+                .clip(CircleShape)
+                .background(if (isChecked) style.selectedBackgroundColor else style.backgroundColor)
+        ) {
+            if(isChecked){
+                Icon(modifier = Modifier.padding(1.dp),
+                    imageVector = Icons.Default.Check, contentDescription = null,
+                tint = style.checkmarkColor
+                    )
+            }
+        }
+        Text(modifier = Modifier.padding(start = 6.dp),
+            text = text,
+        color = style.textColor)
+    }
 }
--- app/src/main/java/com/training/shoplocal/screens/appscreen/ShowFIlterDisplay.kt
+++ app/src/main/java/com/training/shoplocal/screens/appscreen/ShowFIlterDisplay.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.training.shoplocal.classes.fodisplay.OrderDisplay
 import com.training.shoplocal.classes.fodisplay.ProviderDataDisplay
 import com.training.shoplocal.ui.theme.BgScreenDark
 import com.training.shoplocal.ui.theme.PrimaryDark
 import com.training.shoplocal.ui.theme.TextFieldBg
+import com.training.shoplocal.R
 
 const val CATEGORY_ITEM = -1
 const val BRAND_ITEM    = -2
@@ -40,7 +43,7 @@ fun ShowFilterDisplay(filter: ProviderDataDisplay){
 
     @Composable
     fun showDiscount(value: Int){
-        CustomCheckBox(){checked ->
+        CustomCheckBox(text = stringResource(id = R.string.text_favorite), checked = true){ checked ->
 
         }
     }
@@ -60,12 +63,12 @@ fun ShowFilterDisplay(filter: ProviderDataDisplay){
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(0.1f)
-                ,
+                    .weight(0.1f),
                 //elevation = 4.dp,
                 //shape = RoundedCornerShape(20.dp),
                 backgroundColor = PrimaryDark,
             ) {
+                showDiscount(value = 20)
             }
             Spacer(modifier = Modifier.height(8.dp))
             Card(
