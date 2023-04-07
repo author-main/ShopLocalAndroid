477d2823f2f7acc6bbc36083d9b7e3c1ee71e8a5
author-main <author-main@mail.ru>
2023-04-07 11:05:12 +0300

    07/04/2023 реализация Dagger2

--- app/src/main/java/com/training/shoplocal/dagger/AppComponent.kt
+++ app/src/main/java/com/training/shoplocal/dagger/AppComponent.kt
@@ -1,7 +1,16 @@
 package com.training.shoplocal.dagger
 
+import dagger.BindsInstance
 import dagger.Component
 
-@Component
+@Component(modules = [ImageCacheModule::class])
 interface AppComponent {
+    @Component.Builder
+    interface Builder{
+        @BindsInstance
+        fun cacheDir(dir: String): Builder
+        @BindsInstance
+        fun cacheSize(size: Int): Builder
+        fun build(): AppComponent
+    }
 }
--- app/src/main/java/com/training/shoplocal/dagger/ImageCacheModule.kt
+++ app/src/main/java/com/training/shoplocal/dagger/ImageCacheModule.kt
@@ -5,8 +5,9 @@ import dagger.Module
 
 @Module
 class ImageCacheModule {
-    fun provideJournalCache(cacheDirectory: String): Journal     = Journal(cacheDirectory)
-    fun provideDiskCache(cacheDirectory: String): ImageDiskCache = DiskCache(cacheDirectory)
-    fun provideMemoryCache(cacheSize: Int): ImageMemoryCache     = MemoryCache(cacheSize)
-    fun provideImageDownloader(diskCache: ImageDiskCache, memoryCache: ImageMemoryCache) = ImageLinkDownloader(diskCache, memoryCache)
+    fun provideJournalCache     (cacheDirectory: String): Journal        = Journal(cacheDirectory)
+    fun provideDiskCache        (cacheDirectory: String): ImageDiskCache = DiskCache(cacheDirectory)
+    fun provideMemoryCache      (cacheSize: Int): ImageMemoryCache       = MemoryCache(cacheSize)
+    fun provideImageDownloader  (diskCache: ImageDiskCache,
+                                 memoryCache: ImageMemoryCache)          = ImageLinkDownloader(diskCache, memoryCache)
 }
