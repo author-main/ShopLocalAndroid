7ae9021544a2bc43a94beae488452af6e84802d0
author-main <author-main@mail.ru>
2023-04-26 11:28:14 +0300

    26/04/2023 Тестирование Rest API Assured

--- app/src/test/java/com/training/shoplocal/RestAssuredTest.kt
+++ app/src/test/java/com/training/shoplocal/RestAssuredTest.kt
@@ -1,19 +1,17 @@
 package com.training.shoplocal
 
-import com.training.shoplocal.classes.SERVER_URL
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
-import io.restassured.path.json.JsonPath
-import org.hamcrest.Matchers.notNullValue
-import org.junit.Assert
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.matchesPattern
-import org.hamcrest.Matchers.matchesRegex
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RestAssuredTest {
     /*
     Для отображения русской кодировки добавить в studio64.exe.vmoptions:
@@ -21,13 +19,17 @@ class RestAssuredTest {
     -Dfile.encoding=UTF-8
     */
     private val SERVER_TEST_API = "http://192.168.1.10/api/test"
-    @Test
+
+    /*private fun getMail(): Stream<String> = Stream.of("myshansky@inbox.ru")
+    @MethodSource("getMail")*/
+    @ParameterizedTest
+    @ValueSource(strings = ["myshansky@inbox.ru"]) // <- введите email для тестирования
     @DisplayName("Получение токена пользователя по email")
-    fun getUserToken(){
+    fun getUserToken(email: String){
         val token = given()
             .contentType(ContentType.JSON)
-            .get("$SERVER_TEST_API/get_user_token?email=myshansky@inbox.ru")
-            .then().log().all()
+            .get("$SERVER_TEST_API/get_user_token?email=$email")
+            .then()//.log().all()
             .extract().body().jsonPath().getString("token")
         println()
         println(">>> token = $token")
