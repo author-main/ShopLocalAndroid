45c5775e329bb8081585740b97598e65c528c019
Admin <author-main@mail.ru>
2023-06-06 22:05:35 +0300

    03/05/2023 реализация Messages

--- app/src/main/java/com/training/shoplocal/repository/retrofit/DatabaseApi.kt
+++ app/src/main/java/com/training/shoplocal/repository/retrofit/DatabaseApi.kt
@@ -9,46 +9,11 @@ import retrofit2.create
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/*@Singleton
-class RetrofitService @Inject constructor(){
-        private val service: DatabaseApiInterface
-        init{
-            val gson = GsonBuilder()
-                .setLenient()
-                .create()
-            val retrofit = Retrofit.Builder()
-                .baseUrl(SERVER_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
-            service = retrofit.create<DatabaseApiInterface>()
-            //service = retrofit.create(DatabaseApiInterface::class.java)
-        }
-        fun getService() = service
-}
-
-@Singleton
-class DatabaseApi @Inject constructor(private val retrofitApi: RetrofitService){
-    //companion object {
-        private val retrofitService = retrofitApi.getService()*/
 @Singleton
 class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiInterface){
         private val QUERY_REGUSER   = "reg_user"
         private val QUERY_LOGINUSER = "login_user"
         private val QUERY_RESTOREUSER = "restore_user"
-/*    init{
-        val gson = GsonBuilder()
-            .setLenient()
-            .create()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(SERVER_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            //.addConverterFactory(GsonConverterFactory.create())
-            .build()
-        retrofitInstance = retrofit.create(DatabaseApiInterface::class.java)
-    }
-
-    //fun getRetrofitretrofitInstance() = retrofitInstance
-*/
 
         private fun queryUser(query: String, user: User, callback: retrofit2.Callback<User>) {
             val userCall: retrofit2.Call<User> = retrofitService.queryUser(user, query)
@@ -74,9 +39,7 @@ class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiIn
             try {
                 val call: retrofit2.Call<Product> = retrofitService.getProduct(id)
                 call.enqueue(callback)
-            } catch (e: Exception) {
-                //log(e.message ?: "error")
-            }
+            } catch (_: Exception) {}
         }
 
         fun getProducts(
@@ -86,13 +49,12 @@ class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiIn
             callback: retrofit2.Callback<List<Product>>
         ) {
             try {
+                com.training.shoplocal.log(order)
                 val call: retrofit2.Call<List<Product>> = retrofitService.getProducts(token, part, order)
                 call.enqueue(callback)
-            } catch (e: Exception) {
-                //log(e.message ?: "error")
+            } catch (_: Exception) {
             }
         }
-
 
         fun getFoundProducts(
             query: String,
@@ -121,25 +83,7 @@ class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiIn
             }
         }
 
-
-        /*   suspend fun getProducts(id: Int, part: Int) : List<Product> {
-        var list = emptyList<Product>()
-        try {
-            val response = retrofitInstance!!.getProducts(id, part)
-            response.body()?.let{body ->
-                list = body
-            }
-        } catch(e: Exception){
-            //log(e.message ?: "error")
-        }
-        return list
-    }*/
-
         suspend fun updateFavorite(token: String, id_product: Int, value: Byte): Response<Int> {
-            /*try {
-            retrofitInstance!!.updateFavorite(id_user, id_product, value)
-        } catch(_: Exception){
-        }*/
             return retrofitService.updateFavorite(token, id_product, value)
         }
 
@@ -151,18 +95,14 @@ class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiIn
             try {
                 val call: retrofit2.Call<List<Brand>> = retrofitService.getBrands()
                 call.enqueue(callback)
-            } catch (e: Exception) {
-                // log(e.message ?: "error")
-            }
+            } catch (_: Exception) {}
         }
 
         fun getCategories(callback: retrofit2.Callback<List<Category>>) {
             try {
                 val call: retrofit2.Call<List<Category>> = retrofitService.getCategories()
                 call.enqueue(callback)
-            } catch (e: Exception) {
-                // log(e.message ?: "error")
-            }
+            } catch (_: Exception) {}
         }
 
         fun getMessages(token: String,
@@ -171,43 +111,6 @@ class DatabaseApi @Inject constructor(private val retrofitService: DatabaseApiIn
             try {
                 val call: retrofit2.Call<List<UserMessage>> = retrofitService.getMessages(token, requestNumberUnread)
                 call.enqueue(callback)
-            } catch (e: Exception) {
-                // log(e.message ?: "error")
-            }
+            } catch (_: Exception) {}
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-   /* fun createUser(user: User, callback: retrofit2.Callback<User>) {
-        val userCall: retrofit2.Call<User> = retrofitInstance!!.accessUser(user, "reg_user")
-        userCall.enqueue(callback)
-    }
-
-    fun restoreUser(user: User, callback: retrofit2.Callback<User>) {
-        val userCall: retrofit2.Call<User> = retrofitInstance!!.accessUser(user, "restore_user")
-        userCall.enqueue(callback)
-    }
-
-    fun loginUser(user: User, callback: retrofit2.Callback<User>) {
-        val userCall: retrofit2.Call<User> = retrofitInstance!!.accessUser(user, "login_user")
-        userCall.enqueue(callback)
-    }*/
-
-  /*  fun loginUser(user: User, callback: retrofit2.Callback<User>) {
-        val userCall: retrofit2.Call<User> = retrofitInstance!!.createUser(user)
-        userCall.enqueue(callback)
-    }*/
-
-
-
-
-//}
