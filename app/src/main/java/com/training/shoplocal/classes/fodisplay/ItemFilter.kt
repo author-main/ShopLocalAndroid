de8c434729400418ded4481e9ea32569bbeed106
author-main <author-main@mail.ru>
2023-08-31 09:33:58 +0300

    31.08.2023 * final release *

--- app/src/main/java/com/training/shoplocal/classes/fodisplay/ItemFilter.kt
+++ app/src/main/java/com/training/shoplocal/classes/fodisplay/ItemFilter.kt
@@ -7,14 +7,5 @@ import com.training.shoplocal.classes.CATEGORY_ITEM
 data class ItemFilter(
     val id: Int,
     val name: String,
-    //val parent: Int,
     var selected: Boolean = false
-) {
-/*
-    fun isHeader() =
-        id < 0
-    fun isCategory() =
-        id == CATEGORY_ITEM
-    fun isBrand() =
-        id == BREND_ITEM*/
-}
+)
